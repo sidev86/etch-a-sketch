08a7422e2fe67968e7620ed04ab20d73a26c8d2d
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-11-26 07:04:48 +0100

    Fix problem with grid changing width depending on grid size. Now grid is mantaining fixed width

--- etch.js
+++ etch.js
@@ -1,14 +1,45 @@
 // Create a for loop to create the grid
 
-const grid = document.getElementById("grid");
+function drawGrid(gridSize) {
+  const grid = document.getElementById("grid");
+  const gridWidth = grid.clientWidth;
+  console.log(gridWidth);
+  const squareWidth = gridWidth / gridSize;
 
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < 16; j++) {
-    let square = document.createElement("div");
-    square.classList.add("sq");
-    row.appendChild(square);
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < gridSize; j++) {
+      let square = document.createElement("div");
+      square.style.width = squareWidth + "px";
+      square.style.height = squareWidth + "px";
+      square.classList.add("sq");
+      row.appendChild(square);
+    }
+    grid.appendChild(row);
   }
-  grid.appendChild(row);
+
+  // Create a function to change the color of the squares
+  let squares = document.querySelectorAll(".sq");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+  });
 }
+
+function clearGrid() {
+  const grid = document.getElementById("grid");
+  grid.innerHTML = "";
+}
+
+const button = document.querySelector("#btn-generate");
+button.addEventListener("click", () => {
+  let gridSize = prompt("Please enter a grid size. (maximum 100)");
+  if (gridSize > 100 || gridSize < 1) {
+    alert("Please enter a number less than 100");
+    return;
+  }
+  clearGrid();
+  drawGrid(gridSize);
+});
